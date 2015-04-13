5b57e8630566e089838eb8213d3aeaf5c83b5cc4
syiss0257 <galcolover53@gmail.com>
2015-04-13 16:54:11 +0900

    動画解像度変更（4.8bit/pix、2.5Mbps)

--- Library/Sources/SCRecordSession.h
+++ Library/Sources/SCRecordSession.h
@@ -11,7 +11,7 @@
 
 #define kRecordSessionDefaultVideoCodec AVVideoCodecH264
 #define kRecordSessionDefaultVideoScalingMode AVVideoScalingModeResizeAspectFill
-#define kRecordSessionDefaultOutputBitPerPixel 12
+#define kRecordSessionDefaultOutputBitPerPixel 4.8
 #define kRecordSessionDefaultAudioBitrate 128000
 #define kRecordSessionDefaultAudioFormat kAudioFormatMPEG4AAC
 
